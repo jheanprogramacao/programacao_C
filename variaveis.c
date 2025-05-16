0d6aa9722cd8845ac126857e7b1cb1002d5d9350
jheanprogramacao <jhean.santos.5891@gmail.com>
2025-05-16 11:54:33 -0300

    Variáveis

---
+++ variaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 22;
+    int Idade;
+    int quantidade = 2;
+    float altura = 1.73;
+    double peso = 75.5;
+    char letra = 'J';
+    char nome [20] = 'Santos';
+    
+    Idade = 23;
+
+}
